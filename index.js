b698b9b9428c4a45b52c1ec13271bc29a251acca
Bruno Vidal <brunokasprik@gmail.com>
2023-02-16 00:21:48 -0300

    exerc 3.5 step 5

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+app.use(express.json())
 let persons = [
   {
     id: 1,
@@ -53,14 +53,41 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
+  persons = persons.filter((person) => person.id !== id)
 
   response.status(204).end()
 })
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(body)
+
+  
+
+  if (!body.name && !body.number) {
+    return response.status(400).json({
+      error: 'content missing',
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = [...persons, person]
+
+  response.json(person)
+})
+
+const generateId = () => {
+  const min = 100000
+  const max = 999999
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
 
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-
