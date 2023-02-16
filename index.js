65e2f4c0f1759f79440d9ae92641bf67ec3bb0aa
Bruno Vidal <brunokasprik@gmail.com>
2023-02-15 23:47:03 -0300

    exerc 3.4 step 4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: 'Arto Hellas',
@@ -51,7 +51,16 @@ app.get('/api/persons/:id', (request, response) => {
   person ? response.json(person) : response.status(404).end()
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+
