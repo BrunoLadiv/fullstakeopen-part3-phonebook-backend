370dae0512f39b8785b9f5ccada067f2173fe31f
Bruno Vidal <brunokasprik@gmail.com>
2023-02-16 02:33:20 -0300

    exerc 3.8 step 8

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-app.use(morgan('tiny'))
+app.use(morgan('combined'))
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use((req, res, next) => {
+  console.log(req.body)
+  next()
+})
 let persons = [
   {
     id: 1,
