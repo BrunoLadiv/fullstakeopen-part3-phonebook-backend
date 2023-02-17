cdcb51e272ba09a0aebf4bbcb9e7359706dde03b
Bruno Vidal <brunokasprik@gmail.com>
2023-02-17 02:12:10 -0300

    exerc 3.20 step 8

--- models/person.js
+++ models/person.js
@@ -19,7 +19,13 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    required: true,
+    validate: {
+      validator: function (v) {
+         return /^\d{2,3}(-\d{2,3})?\d{5,6}$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required:[true, 'User phone number required'],
   },
 })
 
